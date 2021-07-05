87e2849580174ee6d7d3be4975e6ee947fb81230
Kay Kim <kimjh@bawi.org>
2021-07-05 15:58:34 +0900

    move router change events to inside of useEffect

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
 import '@assets/main.css';
 import 'nprogress/nprogress.css';
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
-import Router from 'next/router';
+import { useRouter } from 'next/router';
 import NProgress from 'nprogress';
 import Script from 'next/script';
 
@@ -18,11 +18,21 @@ NProgress.configure({
   showSpinner: false,
 });
 
-Router.events.on('routeChangeStart', () => NProgress.start());
-Router.events.on('routeChangeComplete', () => NProgress.done());
-Router.events.on('routeChangeError', () => NProgress.done());
-
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.events.on('routeChangeStart', NProgress.start);
+    router.events.on('routeChangeComplete', NProgress.done);
+    router.events.on('routeChangeError', NProgress.done);
+
+    return () => {
+      router.events.off('routeChangeStart', NProgress.start);
+      router.events.off('routeChangeComplete', NProgress.done);
+      router.events.off('routeChangeError', NProgress.done);
+    };
+  }, [router]);
+
   return (
     <>
       <Script src="/js/redirectIE.js" strategy="beforeInteractive" />
